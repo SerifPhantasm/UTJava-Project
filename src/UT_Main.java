da38461330e8b1e807a0c54c4a18c3c57b3ecb75
Person <Person@person.person>
2026-02-19 07:09:34 +0000

    Removed GUI and started over

--- src/UT_Main.java
+++ src/UT_Main.java
@@ -1,101 +1,74 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Scanner;
 
 public class UT_Main {
 
     // base
-    public static void main(String[] args) {
-
-        String username = System.getProperty("user.name");
-        JFrame frame = new JFrame("UT Project");
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
-        frame.setLayout(null);
-        frame.getContentPane().setBackground(Color.BLACK);
-
-        JButton credits = new JButton("About");
-        credits.setBackground(Color.BLACK);
-        credits.setForeground(Color.WHITE);
-
-        credits.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(frame, "Ver 0");
-            }
-        });
-
-        JTextArea outputArea = new JTextArea();
-        outputArea.setBounds(150, 200, 400, 200);
-        outputArea.setEditable(false);
-        outputArea.setBackground(Color.BLACK);
-        outputArea.setForeground(Color.WHITE);
-        JScrollPane outputScroll = new JScrollPane(outputArea);
-
-        JButton process_connect = new JButton("Set");
-        process_connect.setBackground(Color.BLACK);
-        process_connect.setForeground(Color.WHITE);
-        process_connect.setBounds(350, 10, 100, 30);
-        process_connect.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                IntegrityCheck(outputArea, username);
-            }
-        });
+    public static void main(String[] args) throws IOException {
 
 
 
+        // start
+        String file0dir = System.getProperty("user.home") + "\\AppData\\Local\\UNDERTALE\\file0";
+        System.out.println(file0dir);
+        List<String> file0lines = Files.readAllLines(Path.of(file0dir));
+        System.out.println("Press Enter to continue or type exit to quit.");
 
 
-        credits.setBounds(350, 500, 100, 30);
-
-        frame.add(credits);
-        frame.add(process_connect);
-        frame.add(outputArea);
-        frame.add(outputScroll);
-
-        frame.setVisible(true);
-    }
-
-    public static void IntegrityCheck(JTextArea outputArea, String username)
-    {
-        String gamePath = findGamePath();
-
-        outputArea.setText(username + "\n");
-        if (gamePath != null)
+        // enter or exit
+        Scanner enterinp = new Scanner(System.in);
+        String enterinp_str = enterinp.nextLine();
+        if (enterinp_str.equals("exit"))
         {
-            JOptionPane.showMessageDialog(null, "Game found at: " + gamePath);
-            outputArea.append("//" + "\n" + "Path: " + gamePath + "\n");
+            System.out.print("Exiting.");
+            System.exit(0);
         }
-        else
-        {
-            JOptionPane.showMessageDialog(null, "Make sure you have UNDERTALE Installed");
-            outputArea.append("UNDERTALE directory not FOUND." + "\n");
-        }
-    }
 
-    public static String findGamePath()
-    {
-        String UserName = System.getProperty("user.name");
-        String[] gamePath_List = {
-            "C:\\Users\\" + UserName + "\\Downloads\\Undertale\\undertale.exe",
-            "C:\\Users\\" + UserName + "\\Desktop\\Undertale\\undertale.exe",
-            "C:\\Users\\" + UserName + "\\Documents\\Undertale\\undertale.exe",
-            "C:\\Program Files\\Undertale\\undertale.exe",
-            "C:\\Program Files (x86)\\Undertale\\undertale.exe"
-        };
+        System.out.print("Name: ");
+        System.out.println(file0lines.getFirst());
 
-        for (String path : gamePath_List)
-        {
-            java.io.File file = new java.io.File(path);
-            if (file.exists())
-            {
-                System.out.println("Found game at: " + path);
-                return path;
-            }
-        }
-        System.out.println("Game not found in any path.");
-        return null;
+        System.out.print("LV: ");
+        System.out.println(file0lines.get(1));
+
+        System.out.print("MaxHP: ");
+        System.out.println(file0lines.get(2) + "/ " + file0lines.get(2));
+
+        System.out.print("ATTACK: ");
+        System.out.println(file0lines.get(4));
+
+        System.out.print("DEFENSE: ");
+        System.out.println(file0lines.get(6));
+
+        System.out.print("XP: ");
+        System.out.println(file0lines.get(9));
+
+        System.out.print("GOLD: ");
+        System.out.println(file0lines.get(12));
+
+        System.out.print("KILLS: ");
+        System.out.println(file0lines.get(13));
+
+        System.out.print("FUN VALUE: ");
+        System.out.println(file0lines.get(37));
+
+        System.out.print("CURRENT ROOM: ");
+        System.out.println(file0lines.get(547));
+
+        System.out.print("mus_ID: ");
+        System.out.println(file0lines.get(546));
+
+
+
+
+
+
+
+
+
+
+
     }
 }
